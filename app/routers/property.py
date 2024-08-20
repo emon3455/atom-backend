f9e306a493679ec1f5126959731abc7e068e708a
emon <emon.mhk69@gmail.com>
2024-08-20 15:36:23 +0600

    filtering done

--- app/routers/property.py
+++ app/routers/property.py
@@ -5,19 +5,21 @@ from app.database import Session
 router = APIRouter()
 
 @router.get("/filter")
-def filter_data(search_text: str = Query(..., description="Search text for filtering data")):
+def filter_properties_by_search_text(searchText: str = Query(..., description="Search text for filtering data")):
     session = Session()
     try:
-        sql = text("""
-        SELECT * FROM properties
-        WHERE JSON_UNQUOTE(JSON_EXTRACT(address, '$.zip')) LIKE :search_text
-        OR JSON_UNQUOTE(JSON_EXTRACT(address, '$.address')) LIKE :search_text
-        OR JSON_UNQUOTE(JSON_EXTRACT(address, '$.city')) LIKE :search_text
-        OR JSON_UNQUOTE(JSON_EXTRACT(address, '$.state')) LIKE :search_text
-        """)
-        params = {"search_text": f"%{search_text}%"}
+        search_value = f"%{searchText}%"
 
-        result_proxy = session.execute(sql, params)
+        sql = text("""
+            SELECT * FROM location_properties
+            WHERE property_address_postal1 LIKE :searchText
+            OR property_address_postal2 LIKE :searchText
+            OR property_address_postal3 LIKE :searchText
+            OR property_address_oneLine LIKE :searchText
+            OR property_area_countrysecsubd LIKE :searchText
+        """)
+
+        result_proxy = session.execute(sql, {"searchText": search_value})
         results = result_proxy.fetchall()
 
         if results:
@@ -25,29 +27,7 @@ def filter_data(search_text: str = Query(..., description="Search text for filte
             results_list = [dict(zip(columns, row)) for row in results]
             return results_list
         else:
-            raise HTTPException(status_code=404, detail="No matching records found.")
-
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-    finally:
-        session.close()
-
-@router.get("/{property_id}")
-def get_property_by_id(property_id: int = Path(..., description="ID of the property to retrieve")):
-    session = Session()
-    try:
-        sql = text("SELECT * FROM properties WHERE id = :property_id")
-        params = {"property_id": property_id}
-
-        result_proxy = session.execute(sql, params)
-        result = result_proxy.fetchone()
-
-        if result:
-            columns = result_proxy.keys()
-            return dict(zip(columns, result))
-        else:
-            raise HTTPException(status_code=404, detail="Property not found.")
+            return []
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
