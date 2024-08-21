8eff94b1bfde3872d659e1fa7a1aeedc33912d90
emon <emon.mhk69@gmail.com>
2024-08-21 09:41:36 +0600

    connection string updated

--- app/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-db_user = os.getenv("DB_USER")
-db_password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-db_name = os.getenv("DB_NAME")
--- app/database.py
+++ app/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import pymysql
-import app.config as config
 
 pymysql.install_as_MySQLdb()
 
-engine = create_engine(f'mysql+mysqldb://{config.DB_USER}:{config.DB_PASSWORD}@{config.DB_HOST}/{config.DB_NAME}', pool_recycle=3600)
+engine = create_engine(f'mysql+mysqldb://emon:53z5H6qs$@72.167.50.253/locations', pool_recycle=3600)
 Session = sessionmaker(bind=engine)
