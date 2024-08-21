73db2b9219c6b244f6303f8fa5bc4d9496c894f0
emon <emon.mhk69@gmail.com>
2024-08-21 09:51:51 +0600

    trying with environment variable

--- app/database.py
+++ app/database.py
@@ -1,8 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
 
 pymysql.install_as_MySQLdb()
 
-engine = create_engine(f'mysql+mysqldb://emon:53z5H6qs$@72.167.50.253/locations', pool_recycle=3600)
+print(db_user,db_password,db_host,db_name)
+
+engine = create_engine(f'mysql+mysqldb://{db_user}:{db_password}@{db_host}/{db_name}', pool_recycle=3600)
 Session = sessionmaker(bind=engine)
