ecfca592efc863307934b72d421dbef8fdf67ef0
emon <emon.mhk69@gmail.com>
2024-08-22 17:06:57 +0600

    cors issue resolve

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from app.routers import property
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS settings
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
